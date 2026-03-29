b7ae1956a7283635907262375b4831cfe998fe48
a.firsova <a.firsova@propellerads.net>
2026-03-29 17:20:41 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", default=os.getenv("BROWSER"), help="Browser to use")
+    parser.addoption("--browser", default=os.getenv("BROWSER", "chrome"), help="Browser to use")
     parser.addoption("--browser_version", default=os.getenv("BROWSER_VERSION", "127.0"), help="Browser version")
     parser.addoption("--headless", default=os.getenv("HEADLESS", "False"), help="Headless mode True/False")
     parser.addoption("--width", default=os.getenv("SCREEN_WIDTH", "1440"), help="Window width")
